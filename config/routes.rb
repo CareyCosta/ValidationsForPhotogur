e669f647cbdc9f0f44965020b7250efd719947bf
Carey Costa <careyxcosta@gmail.com>
2017-01-27 13:29:01 -0500

    creating picture model in database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'pictures' => 'pictures#index'
+  #match any HTTP GET request for the URL /pictures to the index action in PicturesController
 
 end
