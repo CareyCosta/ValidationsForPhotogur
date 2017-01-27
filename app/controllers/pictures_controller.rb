7357a360763f60555a82a11f68240d7bc263be3f
Carey Costa <careyxcosta@gmail.com>
2017-01-27 15:47:51 -0500

    complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,15 +13,16 @@ class PicturesController < ApplicationController
   end
 
   def create
-    # make a new picture with what picture_params returns (which is a method we're calling)
-   @picture = Picture.new(picture_params)
-   if @picture.save
-     # if the save for the picture was successful, go to index.html.erb
-     redirect_to pictures_url
-   else
-     # otherwise render the view associated with the action :new (i.e. new.html.erb)
-     render :new
-   end
+      # make a new picture with what picture_params returns (which is a method we're calling)
+     @picture = Picture.new(picture_params)
+     if @picture.save
+       # if the save for the picture was successful, go to index.html.erb
+       redirect_to pictures_url
+     else
+       # otherwise render the view associated with the action :new (i.e. new.html.erb)
+       render :new
+     end
+  end
 
    def edit
      @picture = Picture.find(params[:id])
@@ -42,11 +43,9 @@ class PicturesController < ApplicationController
      @picture.destroy
      redirect_to pictures_url
    end
-end
 
-private
-def picture_params
-  params.require(:picture).permit(:artist, :title, :url)
-end
-
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  root 'pictures#index'
-  resources :pictures
 
-get 'pictures' => 'pictures#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'pictures' => 'pictures#index'
   #match any HTTP GET request for the URL /pictures to the index action in PicturesController
-post'pictures' => 'pictures#create'
 get 'pictures/new' => 'pictures#new'
-get 'pictures/:id' => 'pictures#show', as: 'picture'
 get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+post'pictures' => 'pictures#create'
 patch 'pictures/:id' => "pictures#update"
 delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 
 root 'pictures#index'
 
+resources :pictures
 end
