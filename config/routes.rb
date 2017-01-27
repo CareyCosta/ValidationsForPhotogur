790a69a24ea28c0a3d38e3a3e6837bfa2fa619b4
Carey Costa <careyxcosta@gmail.com>
2017-01-27 13:43:28 -0500

    Making each picture show at its own URL with ynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'pictures' => 'pictures#index'
   #match any HTTP GET request for the URL /pictures to the index action in PicturesController
-
+get 'pictures/:id' => 'pictures#show', as: 'picture'
 end
